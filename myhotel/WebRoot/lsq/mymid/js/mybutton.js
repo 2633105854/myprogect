c435166e81131baa2b64aab4073279f7da434323
青莲剑歌 <2633105854@qq.com>
2016-09-28 18:57:56 +0800

    Revert "Revert "Revert "myhotel"""
    This reverts commit e491709921ac08437deb41b6d7bd1cc560299c22.

--- myhotel/WebRoot/lsq/mymid/js/mybutton.js
+++
@@ -1,6 +0,0 @@
-function danji()
-{
-  
-   window.location.href="http://localhost:8080/myhotel/lsq/mymid/mymid.html";
-}
-
--- myhotel/WebRoot/lsq/mymid/js/myjs.js
+++
@@ -1,5 +0,0 @@
-function danji_xiala()
-{
-	var myd=document.getElementById("ss");
-	myd.slideUp(1000);
-}

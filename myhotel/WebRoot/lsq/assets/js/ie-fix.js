6ac92bbd63edea714902d4c4ec1ae994ea2e988e
青莲剑歌 <2633105854@qq.com>
2016-09-28 16:19:18 +0800

    Revert "myhotel"
    This reverts commit 7ee5995321d78454c2e4c317c23be743a5cc0669.

--- myhotel/WebRoot/lsq/assets/js/ie-fix.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-  $('ul').each(function() {
-    $(this).find('> li:last').addClass('ie_last_child');
-  });
-  $('.bg-header-menu ul.menu li a').addClass('border-menu');
-});
